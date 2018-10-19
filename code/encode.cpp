da1f486e3134a296e61ad6d7458f9c747c766ee9
Trevor Ballard <trevorcoleballard@gmail.com>
2018-10-19 11:30:14 +0200

    Convert to bits then back to bytes, just duplicate the file

--- code/encode.cpp
+++ code/encode.cpp
@@ -7,24 +7,31 @@
 typedef unsigned char Block;
 typedef boost::dynamic_bitset<Block> Bitset;
 
-int getNextNAL(std::ifstream& ifs, unsigned char* buf) {
+int getNextNAL(std::ifstream& ifs, std::vector<Block>* buf) {
 	// Go past the first border and consume it.
 	int zeroCounter = 0;
 	char c = 0xFF;
 	int bufIdx = 0;
-	while (zeroCounter < 2 || (unsigned char)c != 0x01) {
+	while ((zeroCounter < 2 || (unsigned char)c != 0x01)
+		   && ifs.peek() != std::ifstream::traits_type::eof()) {
 		ifs.read(&c, 1);
-		buf[bufIdx] = (unsigned char)c;
+		//buf[bufIdx] = (unsigned char)c;
+		buf->push_back((unsigned char)c);
 		bufIdx++;
 		if ((unsigned char)c == 0x00) {
 			zeroCounter++;
 		}
 	}
+	if (ifs.peek() == std::ifstream::traits_type::eof()) {
+		return -1;
+	}
 	// Stop when we encounter the next border. Do not consume it.
 	zeroCounter = 0;
-	while (zeroCounter < 2 || (unsigned char)c != 0x01) {
+	while ((zeroCounter < 2 || (unsigned char)c != 0x01)
+		   && ifs.peek() != std::ifstream::traits_type::eof()) {
 		ifs.read(&c, 1);
-		buf[bufIdx] = (unsigned char)c;
+		//buf[bufIdx] = (unsigned char)c;
+		buf->push_back((unsigned char)c);
 		bufIdx++;
 		if ((unsigned char)c == 0x00) {
 			zeroCounter++;
@@ -33,31 +40,26 @@ int getNextNAL(std::ifstream& ifs, unsigned char* buf) {
 			zeroCounter = 0;
 		}
 	}
+	if (ifs.peek() == std::ifstream::traits_type::eof()) {
+		return -1;
+	}
 	ifs.putback((unsigned char) 0x01);
 	ifs.putback((unsigned char) 0x00);
 	ifs.putback((unsigned char) 0x00);
+	buf->pop_back();
+	buf->pop_back();
+	buf->pop_back();
 	return bufIdx-3;
 }
 
-void appendCharToBitset(boost::dynamic_bitset<Block>* bits, unsigned char c) {
-	for (int i=0; i<8; i++) {
-		bits->push_back((c>>i) & 1);
-	}
-}
+int modifySPS(std::vector<Block>* nal, int nalLen) {
+	// TODO calculate new size
+	Bitset oldBits(spsLen*8);
+	Bitset newBits((spsLen+50)*8);
 
-int modifySPS(unsigned char* nal, int nalLen) {
-	//boost::dynamic_bitset<> x(999);
-	Bitset x(999);
-	printf("%d\n", nalLen);
-	// Convert bytes to bitset
-	for (int i=0; i<nalLen; i++) {
-		appendCharToBitset(&x, nal[i]);
-	}
-
-	// Convert bitset to bytes
-	//boost::to_block_range(&x, std::back_inserter(nal));
-	std::vector<Block> bytes;
-	boost::to_block_range(x, std::back_inserter(bytes));
+	boost::from_block_range(nal->begin(), nal->end(), x);
+	nal->clear();
+	boost::to_block_range(x, std::back_inserter(*nal));
 	//ofs.write((char*), 999);
 	return x.size();
 }
@@ -65,20 +67,25 @@ int modifySPS(unsigned char* nal, int nalLen) {
 int main(int, char*[]) {
 	std::ifstream ifs("../../videos/stitched.hevc", std::ios::binary);
 	std::ofstream ofs("test.hevc", std::ios::binary);
-	unsigned char rawNAL[99999];
+	std::vector<Block> rawNAL;
 
-	int vpsLen = getNextNAL(ifs, rawNAL);
-	ofs.write((char*)rawNAL, vpsLen);
+	int vpsLen = getNextNAL(ifs, &rawNAL);
+	ofs.write((char*)&rawNAL[0], rawNAL.size());
+	rawNAL.clear();
 
-	int spsLen = getNextNAL(ifs, rawNAL);
-	ofs.write((char*)rawNAL, spsLen);
-	printf("%d\n", modifySPS(rawNAL, spsLen));
+	int spsLen = getNextNAL(ifs, &rawNAL);
+	modifySPS(&rawNAL, spsLen);
+	ofs.write((char*)&rawNAL[0], rawNAL.size());
+	rawNAL.clear();
 
-	int ppsLen = getNextNAL(ifs, rawNAL);
-	ofs.write((char*)rawNAL, ppsLen);
+	int ppsLen = getNextNAL(ifs, &rawNAL);
+	ofs.write((char*)&rawNAL[0], ppsLen);
+	rawNAL.clear();
 
-	while (true) {
-		int nalLen = getNextNAL(ifs, rawNAL);
-		ofs.write((char*)rawNAL, nalLen);
+	int nalLen = 0;
+	while (nalLen != -1) {
+		nalLen = getNextNAL(ifs, &rawNAL);
+		ofs.write((char*)&rawNAL[0], nalLen);
+		rawNAL.clear();
 	}
 }
